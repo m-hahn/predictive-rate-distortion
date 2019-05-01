714fa3bca261b7ddc8c1855ab98ae6e8e4fa7cb4
Michael Hahn <mhahn29@gmail.com>
2019-04-30 18:02:51 -0700

    make complexity precise

--- code/analysis/analyze_RIP.R
+++ code/analysis/analyze_RIP.R
@@ -60,8 +60,9 @@ dataNU = dataNU[order(dataNU$EE),]
 
 
 plot = ggplot(dataU, aes(x=MiWithFut, y=Memory, group=Type, color=Type))
-plot = plot + geom_point()+ theme_classic() + xlim(0,NA) + ylim(0, 1.1) 
-plot = plot + geom_line(data=data.frame(x=c(0, 0.6365, 0.8595025), y = c(0, 0.6365, 1.039721)), aes(x=x , y=y, group=NA, color=NA), size=4) 
+plot = plot + geom_point()+ theme_classic() + xlim(0,NA) + ylim(0, 1.1)
+# Plot curve as computed from p. 1328-9 in Marzen and Crutchfield (2019).
+plot = plot + geom_line(data=data.frame(x=c(0, 0.6365, 0.8595025), y = c(0, 0.6365, 1.05492)), aes(x=x , y=y, group=NA, color=NA), size=4) 
 plot = plot + theme(legend.position="none") 
 plot = plot + geom_line(data=findFrontier(dataNU$EE, dataNU$Memory), aes(x=EE, y=Memories, group=NULL, color=NULL), colour="red", size=2.5)
 plot = plot +    theme(    axis.text.x = element_text(size=20),
