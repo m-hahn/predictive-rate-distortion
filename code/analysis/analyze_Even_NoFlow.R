bf3be2c48247a826a506905bb5f45f7d94e696d4
Michael Hahn <mhahn29@gmail.com>
2019-06-17 16:25:46 +0200

    minor viz bug

--- code/analysis/analyze_Even_NoFlow.R
+++ code/analysis/analyze_Even_NoFlow.R
@@ -32,7 +32,6 @@ d = merge(d,d2, by=c("BetaD", "Objective"))
 
 dataU = data %>% filter(Language == "even") %>% mutate(Type = "Neural")
 
-source("findFrontier.R")
 
 
 
@@ -52,11 +51,12 @@ dataNU = dataU %>% filter(Type == "Neural")
 dataNU = dataNU[order(dataNU$EE),]
 
 
+source("findFrontier.R")
 plot = ggplot(dataU, aes(x=MiWithFut, y=Memory, group=Type, color=Type))
 plot = plot + geom_point()+ theme_classic() + xlim(0,NA) + ylim(0, 1.1) 
 plot = plot + geom_line(data=data.frame(x=c(0, 0.6365), y = c(0, 0.6365)), aes(x=x , y=y, group=NA, color=NA), size=4) 
 plot = plot + theme(legend.position="none") 
-plot = plot + geom_line(data=findFrontier(dataNU$EE, dataNU$Memory), aes(x=EE, y=Memories, group=NULL, color=NULL), colour="red", size=2.5)
+plot = plot + geom_line(data=findFrontier(dataNU$MiWithFut, dataNU$Memory), aes(x=EE, y=Memories, group=NULL, color=NULL), colour="red", size=2.5)
 plot = plot +    theme(    axis.text.x = element_text(size=20),
 		           axis.text.y = element_text(size=20),
 			   axis.title.x = element_text(size=25),
